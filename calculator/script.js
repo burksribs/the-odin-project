8d661ba8e67945785d43cdb1b6b203cd95ffe624
burksribs <burksribs@outlook.com>
2022-04-27 18:28:26 +0300

    calculator done

--- calculator/script.js
+++ calculator/script.js
@@ -11,34 +11,76 @@ let lhs = null;
 let rhs = null;
 let operator = null;
 let result = 0;
-let oldOp = operator;
-
 
 Array.from(buttons).forEach((button) => {
     button.addEventListener('click', event => {
-        if (numbers.includes(button.textContent)){
+        if (numbers.includes(button.textContent) && input.textContent.length < 25){
             input.textContent += button.textContent;
+            rhs = input.textContent;
         }
-        else if (operators.includes(button.textContent))        {
-            if (operators.includes(input.textContent.slice(-1))){
-                {
-                    input.textContent = replaceLastChar(input.textContent, button.textContent);
-                    oldOp
+        else if (operators.includes(button.textContent)){
+            expression.textContent += input.textContent + button.textContent;
+
+            if (numOp(expression.textContent) > 1){
+                if (input.textContent != ""){
+                    result = operate(operator, lhs, rhs);
+                    expression.textContent = result + button.textContent;
+                    lhs = result;
                 }
-                
             }
-            else
-                input.textContent += button.textContent;
+            else{
+                lhs = expression.textContent.slice(0,-1);
+            }
+
+            input.textContent = "";
             operator = button.textContent;
         }
         else if (button.textContent == "="){
-
+            if (input.textContent != ""){
+                result = operate(operator, lhs, rhs);
+                input.textContent = result;
+                expression.textContent = "";
+            }
         }     
-            
-        
-            
+        else if (button.textContent == "CLEAR"){
+            result = 0;
+            expr = "";
+            lhs = null;
+            rhs = null;
+            operator = null;
+            expression.textContent = "";
+            input.textContent = "";
+        }
+        else if (button.textContent == "DELETE"){
+            input.textContent = input.textContent.slice(0,-1);
+        }
+        else if (button.textContent == "."){
+            if(numDot(input.textContent) < 1)
+                input.textContent += ".";
+            console.log(numDot(input.textContent));
+        }
     });
 });
+
+function numDot(str)
+{
+    let count = 0;
+    for (let i = 0; i < str.length; i++){
+        if (str[i] == '.')
+            count++;
+    }
+    return count;
+}
+
+function numOp(str){
+    let count = 0;
+    for (let i = 0; i < str.length; i++){
+        if (str[i] == '+' || str[i] == '-' || str[i] == '/' || str[i] == '*'){
+            count++;
+        }
+    }
+    return count;
+}
 
 function replaceLastChar(str, ch){
     let replaced = str.slice(0,-1) + ch;
